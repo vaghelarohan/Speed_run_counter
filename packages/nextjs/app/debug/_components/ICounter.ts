c031254b3141d7ffcce2cbb9efa840aef7203256
vaghelarohan <vaghelarohan366@gmail.com>
2025-08-02 06:58:09 +0000

    counter

--- packages/nextjs/app/debug/_components/ICounter.ts
+++ packages/nextjs/app/debug/_components/ICounter.ts
@@ -1,7 +1,7 @@
 export const ICounter = [
-    "function number() view returns (uint256)",
-    "function setNumber(uint256 new_number)",
-    "function mulNumber(uint256 new_number)",
-    "function addNumber(uint256 new_number)",
-    "function increment()"
+  "function number() view returns (uint256)",
+  "function setNumber(uint256 new_number)",
+  "function mulNumber(uint256 new_number)",
+  "function addNumber(uint256 new_number)",
+  "function increment()",
 ];
